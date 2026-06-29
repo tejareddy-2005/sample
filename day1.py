2be5240eda25eecad9be3b704c19ce970bd7cb1e
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-06-29 23:44:37 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,93 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================= RESTART: C:/Users/Sita/Desktop/python/day1.py ================
+6
+>>> print(3+3)
+6
+>>> print(a)
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined
+>>> b=2
+>>> c=4
+>>> print(b+c)
+6
+>>> x=10
+>>> pritn(x)
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    pritn(x)
+NameError: name 'pritn' is not defined. Did you mean: 'print'?
+>>> print(x)
+10
+>>> z=40print(z)
+SyntaxError: invalid decimal literal
+>>> z=40
+>>> print(z)
+40
+>>> a3=20
+>>> print(a3)
+20
+>>> name="teja"
+>>> print(name)
+teja
+>>> city='vij'
+>>> print(city)
+vij
+>>> country='india'
+>>> print(country)
+india
+>>> _=20
+>>> print(_)
+20
+>>> _b=30
+>>> print(_b)
+30
+mailid="tejareddydusanio@gmail.com"
+print(mailid)
+tejareddydusanio@gmail.com
+a=4,b=5
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+a=4;b=5
+print(a+b)
+9
+a=b=c=d=20
+print(a+b+c+d)
+80
+a,b,c,d=(1,2,3,4)
+print(a,b,c,d)
+1 2 3 4
+fist_name="teja"
+print(first_name)
+Traceback (most recent call last):
+  File "<pyshell#33>", line 1, in <module>
+    print(first_name)
+NameError: name 'first_name' is not defined. Did you mean: 'fist_name'?
+first_name="teja"
+print(first_name)
+teja
+firstname="teja"
+print(firstname)
+teja
+fname+"teja"
+Traceback (most recent call last):
+  File "<pyshell#38>", line 1, in <module>
+    fname+"teja"
+NameError: name 'fname' is not defined. Did you mean: 'name'?
+fname="teja
+SyntaxError: incomplete input
+fname="teja"
+sname="reddy"
+print(fname+sname)
+tejareddy
+NAME="teja"
+print(NAME)
+teja
+Name="teja"
+print(Name)
+teja
+_0=20
+print(_0)
+20
