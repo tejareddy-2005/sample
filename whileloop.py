7fd8944489dc7ae09404189f5147d34c6fa8ad7f
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-07-16 21:38:39 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,84 @@
+#while loop
+'''a=10
+while a>1:
+    print(a)'''
+
+'''a=10
+while a<1:
+    print(a)'''
+
+'''a=10
+while a>1:
+    print(a)
+    a=a-1'''
+
+'''a=10
+while a>=1:
+    print(a)
+    a=a-1'''
+'''a=20
+while a>3:
+    a=a-1
+    print(a)'''
+
+'''a=20
+while a>3:
+    a=a-1
+print(a)'''
+
+'''a=40
+while a>5:
+    a=a-1
+print(a)'''
+
+'''a=30
+while a>1:
+    print(a)
+    a+=1'''
+
+'''a=10
+while a>1:
+    print(a)
+    a-=1'''
+'''a=30
+while a>1:
+    print(a)
+    a=-1'''
+
+'''a=1
+while a<25:
+    print(a)
+    a+=1'''
+
+'''while True:
+    age=int(input("enter age:"))
+    if age>=18:
+        print("eligible")
+    else:
+        print("not eligible")'''
+#range := the range function return sequence of number , starting from 0 by default and icrements by one by one and start before a specified number.
+'''for i in range(20):
+    print(i)'''
+
+'''for i in range(13,25):
+    print(i)'''
+
+'''for i in range(0,30,3):
+    print(i)'''
+'''for i in range(0,50,5):
+    print(i)'''
+'''for i in range(0,20,2):
+    print(i)'''
+
+'''n=int(input("enter marks:"))
+if n in range(91,101):
+    print("grade a")
+elif n in range(81,91):
+    print("grade b")
+elif n in range (71,81):
+    print("grade c")
+elif n in range (50,71):
+    print("grade d")
+else:
+    print("fail")'''
+
