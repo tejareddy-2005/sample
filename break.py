84ee10a79725a9017093bc2cded2586f190b7c5a
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-07-16 21:41:07 +0530

    Add files via upload

---
+++ break.py
@@ -0,0 +1,63 @@
+#break
+'''a=10
+while a>1:
+    a=a-1
+    if a==6:
+        break
+    print(a)'''
+
+'''for i in range(20):
+    if i==13:
+        break
+    print(i)'''
+
+'''a="python"
+if a=="h":
+    break
+print(a)''' #error
+
+'''a="python"
+for i in a:
+    if i=="h":
+        break
+    print(i)'''
+
+#continue
+'''a=20
+while a>5:
+    print(a)
+    a=a-1
+    if a==10:
+        continue'''
+
+'''a=20
+while a>5:
+    a=a-1
+    if a==10:
+        continue
+    print(a)'''
+
+'''for i in range(15):
+    if i==7:
+        continue
+    print(i)'''
+
+'''a="python"
+for i in a:
+    if i=="o":
+        continue
+    print(i)'''
+
+#pass
+'''a=30
+while a>10:
+    print(a)
+    a=a-1
+    if a==20:
+        pass'''
+
+'''for i in range(40):
+    if i==10:
+        pass
+    print(i)'''
+
