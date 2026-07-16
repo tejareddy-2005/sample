58b32a914fd2bb8de85a955d11f307dcaf97cada
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-07-16 21:42:32 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,72 @@
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(a)'''
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+    print(type(a))
+    print(type(i))'''
+
+
+'''a=[10,20,30,40,50]
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''    
+
+'''a={1,2,3,4,5}
+for i in a:
+    print(i)'''
+
+'''a={1,2,3,4,5}
+for i in a:
+    print(i)
+print(type(a))
+print(type(i))'''
+
+'''b={"year":2026,"month":"july","date":9}
+for i in b:
+    print(i)'''
+
+'''b={"year":2026,"month":"july","date":9}
+for i in b:
+    print(i)
+print(type(b))
+print(type(i))'''
+
+'''a="codegnan"
+for i in a:
+    print(i)'''
+
+'''b=[4.5,6.7,8.9]
+for i in b:
+    print(i)'''
+
+'''b=[4.5,6.7,8.9]
+for i in b:
+    print(i)
+    print(type(b))
+    print(type(i))'''
+
+'''b=[4.5,6.7,8.9]
+for i in b:
+    print(i)
+print(type(b))
+print(type(i)) '''
+
+'''b=[4+9j,3+7j]
+for i in b:
+    print(i)'''
+
+'''b=[4+9j,3+7j]
+for i in b:
+    print(i)    
+print(type(b))
+print(type(i))'''
+
+
