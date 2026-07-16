5f11107d2e513422a71e5fde5350af750488d6b1
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-07-16 21:41:30 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,123 @@
+'''a=10
+b=20
+if a<b:
+    print("true")'''
+'''a=10
+b=20
+if a>b:
+    print("true")'''
+'''a=5
+b=7
+if a<=b:
+    print("less")'''
+'''a=12
+b=15
+if a>=b:
+    print("true")'''
+'''a=10
+b=10
+if a==b:
+    print("true")'''
+'''a="python"
+b="python"
+if a==b:
+    print("match")'''
+'''a=int(input("a value :"))
+b=int(input("b value :"))
+if a<b:
+    print("less")'''
+'''a=int(input("a value :"))
+if a>50:
+      print("true")'''
+#if condition by using logical operators
+#and,or,not
+'''a=3
+b=6
+if a<b and b>a:
+    print("true")'''
+'''a=4
+b=7
+if a<=b and b>=a:
+    print("true")'''
+'''a=9
+b=12
+if a!=b and a==b:
+    print("true")'''
+'''a=2
+b=4
+if a<b or a>b:
+    print("true")'''
+'''a=14
+b=16
+if a<=b or b>=a:
+    print("true")'''
+'''a=3
+b=6
+if a!=b or a==b:
+    print("true")'''
+'''a=5
+b=7
+if not a<b:
+    print("true")'''
+'''a=3
+b=6
+if not a<b and b>a:
+    print("true")'''
+'''a=3
+b=6
+if not a<b or b>a:
+    print("true")'''
+# if-condition by using identify operators
+# is,is not
+'''a=4
+if type(a)  is int:
+    print("is is int")'''
+'''a=4
+if type(a) is not int:
+    print("is is int")'''
+'''a="babu"
+if type(a) is str:
+    print("is str")'''
+'''a=3.4
+if type(a) is float:
+    print(" is float")'''
+#if condition by using membership operators
+'''a=1,2,3,4,5,6,7,8
+if 8 in a:
+    print("true")'''
+'''a=2,3,4,5,6,7,8
+if 20 not in a:
+    print("true")'''
+'''a=int(input("a value"))
+if 30 in a:
+    print("true")'''#error
+'''a=2,3,4,5,6,7,8,9,10
+b=int(input("enter a value :"))
+if b in a:
+    print("true")'''
+#examples if-else condition by using comparision operators
+'''a=4
+b=5
+if a<b:
+    print("less")
+else:
+    print("false")'''
+'''a=4
+b=5
+if a>b:
+    print("less")
+else:
+    print("false")'''
+'''a=2
+b=3
+if a==b:
+    print("equal")
+else:
+    print("not equal")'''
+'''a=4
+b=5
+if a!=b:
+    print("true")
+else:
+    print("false")'''
+
