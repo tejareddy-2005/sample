2128b813d5021c969f7cd636c2fa402ee1b4e586
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-07-24 19:57:23 +0530

    Add files via upload

---
+++ tasks.py
@@ -0,0 +1,55 @@
+#ticket=1000
+#gender-male  or famale
+
+'''while True:
+    def ticket():
+        print("====Railway Ticket Booking====")
+        price=1000
+        gender=input'''('''choose the gender
+                        1.male
+                        2.female :''')'''
+        if gender=="1":
+            gender="male"
+        else:
+            gender=="female"
+        age=int(input("enter age:"))
+        if gender=="male":
+            if age>=60:
+                price=price*0.70
+                print("30% A")
+            else:
+                print("ND applied")
+                print(price)
+        if gender=="female":
+            if age>=60:
+                price=price*0.65
+                print("35% AD")
+            else:
+                price=price*0.70
+                print("30% AD")
+        print("total price: rs",int(price))
+    ticket()'''
+
+
+#Marks-analysis Report
+'''a=int(input("enter no of student :"))
+m=[]
+for i in range(1,a+1):
+    ma=int(input(f"enter the student{i} marks"))
+    marks.append(ma)
+for i in ma:
+    print(i)
+print("----------- marks analyst report---------")
+print(a)
+print(max(m))
+print(min(m))
+print(sum(m))
+print((sum/n))'''
+
+'''u="teja"
+p=1234
+u=input("enter name:")
+if u=="teja":
+    print("valid")
+elif u!="teja":
+    print("in valid")'''
