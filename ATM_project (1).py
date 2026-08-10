49cce977fd078e8caffb28b5e24246cb69f3ac3e
DUSANI TEJA REDDY <tejareddydusani0@gmail.com>
2026-08-10 14:46:06 +0530

    Add files via upload

---
+++ ATM_project (1).py
@@ -0,0 +1,25 @@
+#ATM APPLICATION
+'''while True:
+    account=100000
+    pwd=1234
+    card=input("Isert the card: ")
+    if card=="c":
+        print("Welcome Jaswanth")
+        password=int(input("Enter the password: "))
+        if password==pwd:
+            option=int(input(choose the option
+                                1.balance enq
+                                2.withdraw))
+            if option==1:
+                print("Account Balance is: ",account)
+            elif option==2:
+                money=int(input("Enter the amount: "))
+                print(money)
+                balance=account-money
+                print("Remaning Account balance is: ",balance)
+            else:
+                print("Invalid Option")
+        else:
+            print("Incorrect Password")
+    else:
+        print("Invalid Card")'''
